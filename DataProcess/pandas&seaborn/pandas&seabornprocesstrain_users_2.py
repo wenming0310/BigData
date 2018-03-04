ef44abb1b20a889650bd852994e784bb01d4dbb3
wenming0310 <474196608@qq.com>
2018-03-04 22:41:41 +0800

    2018年3月4日22:41:39

---
+++ DataProcess/pandas&seaborn/pandas&seabornprocesstrain_users_2.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import pandas
+import seaborn
+users = pandas.read_csv("train_users_2.csv")
+
+#commdehead = users.head(3)
+#commde = users.tail()
+#commde = users.describe()   #显示数据属性的数据
+#commde = users.shape
+#commde = users.loc[:3, 'age']   #:3从0到3 1:3从1到3
+#commde = users["age"]
+
+#users["date_account_created"] = pandas.to_datetime(users["date_account_created"])  #格式转换
+#commde = users.loc[0,"date_account_created"] - users.loc[1,"date_account_created"]
+
+#users["timestamp_first_active"] = pandas.to_datetime(users["timestamp_first_active"],format="%Y%m%d%H%M%S")
+#commde = users["timestamp_first_active"]
+
+commde = users["age"].dropna()
+
+#print(commdehead)
+#print(commde)
+
+"""
+%matplotlib inline #在jupyter中显示图像
+seaborn.distplot(commde)
+seaborn.boxplot(commde)
+users_with_true_age = users[users["age"]<90]
+#seaborn.boxplot(users_with_true_age["age"])
+users_with_true_age = users[users["age"]<90]
+users_with_true_age = users_with_true_age[users_with_true_age["age"]>10]
+seaborn.boxplot(users_with_true_age["age"])
+seaborn.distplot(users_with_true_age["age"])
+"""
+#seaborn.distplot(commde)
+#seaborn.boxplot(commde)
+users_with_true_age = users[users["age"]<90]
+#seaborn.boxplot(users_with_true_age["age"])
+users_with_true_age = users[users["age"]<90]
+users_with_true_age = users_with_true_age[users_with_true_age["age"]>10]
+#seaborn.boxplot(users_with_true_age["age"])
+seaborn.distplot(users_with_true_age["age"])
+plt.show()
